42636ccdb611c9cfbc7a5123386e6745465f96ca
R. H. Bayu Prabowo <rh.bayu.prabowo@outlook.com>
2017-11-15 02:02:11 +0700

    Edit
    -add regex

--- js/main.js
+++ js/main.js
@@ -3,6 +3,11 @@ document.getElementById('myForm').addEventListener('submit', simpanBookmark);
 function simpanBookmark(e) {
   var siteName = document.getElementById('namaSitus').value;
   var siteUrl = document.getElementById('linkSitus').value;
+
+  if (!validateForm(siteName, siteUrl)) {
+    return false;
+  }
+
   var bookmark = {
     name: siteName,
     url: siteUrl
@@ -44,7 +49,7 @@ function muatBookmark() {
     var url = bookmarks[i].url;
     hasilBookmarks.innerHTML += '<div class="card">';
     hasilBookmarks.innerHTML += '<div class="card-body"><h3>' + name +
-      '<a class="btn btn-primary" target="_blank" href="http://' + url + '">Kunjungi</a>' +
+      '<a class="btn btn-primary" target="_blank" href="' + isiHTTP(url) + url + '">Kunjungi</a>' +
       '<a onclick="hapusBookmark(\'' + url + '\')" class="btn btn-danger"  href="#">Hapus</a>' +
       '</h3>' +
       '</div>';
@@ -66,4 +71,30 @@ function hapusBookmark(url) {
   localStorage.setItem('bookmarks', JSON.stringify(bookmarks));
   //muat ulang
   muatBookmark();
+}
+
+function validateForm(siteName, siteUrl) {
+  //cek if siteName or siteUrl empty
+  if (!siteName || !siteUrl) {
+    alert('Nama Situs atau Link Situs tidak boleh kosong!');
+    return false;
+  }
+
+  var expression = /^(http:\/\/www\.|https:\/\/www\.|http:\/\/|https:\/\/)?[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?(\/.*)?$/gm;
+  var regex = new RegExp(expression);
+
+  if (!siteUrl.match(regex)) {
+    alert('Tolong isi link yang valid');
+    return false;
+  }
+
+  return true;
+}
+
+function isiHTTP(siteUrl) {
+  if (!siteUrl.startsWith('http://') || !siteUrl.startsWith('https://')) {
+    return 'http://'
+  } else {
+    return '';
+  }
 }
